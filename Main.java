9a7d0f7b255a4f0524c717a61184b448c7f212fa
yasiru2077 <96358931+yasiru2077@users.noreply.github.com>
2025-11-28 14:31:34 +0530

    completed this tutoreial

--- Main.java
+++ Main.java
@@ -1,4 +1,28 @@
 public class Main {
 
+    public static void main(String[] args){
+        Second enemy = new Second(25,"daffy");
+        Second enemy2 = new Second();
 
+        System.out.println("enemy is: "+enemy.enemyName + ". enemy level is: "+enemy.enemyLevel);
+        System.out.println("enemy is: "+enemy2.enemyName + ". enemy level is: "+enemy2.enemyLevel);
+
+
+    }
+
+}
+
+class Second {
+
+    int enemyLevel;
+    String enemyName;
+
+    public Second() {
+        this(24,"caviar");
+    }
+
+    public Second(int enemyLevel, String enemyName) {
+        this.enemyLevel = enemyLevel;
+        this.enemyName = enemyName;
+    }
 }

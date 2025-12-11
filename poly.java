1caed439dcfee4f5d0eb98bb52ad344ec88e02c8
yasiru2077 <96358931+yasiru2077@users.noreply.github.com>
2025-12-11 23:32:58 +0530

    adding polymorphism

--- poly.java
+++ poly.java
@@ -1,2 +1,33 @@
-public class poly {
+class Animal {
+    public void animalSound() {
+        System.out.println("The animal makes a sound");
+    }
 }
+
+class Pig extends Animal {
+    public void animalSound() {
+        System.out.println("The pig says: wee wee");
+    }
+}
+
+class Dog extends Animal {
+    public void animalSound() {
+        System.out.println("The dog says: bow wow");
+    }
+}
+
+public class poly {
+
+    public static void main(String[] args){
+        Animal myAnimal = new Animal();  // Create a Animal object
+        Animal myPig = new Pig();  // Create a Pig object
+        Animal myDog = new Dog();  // Create a Dog object
+        myAnimal.animalSound();
+        myPig.animalSound();
+        myDog.animalSound();
+
+    }
+
+}
+
+

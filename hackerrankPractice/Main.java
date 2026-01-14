85d5674e4ec2e2750668eea58c0a6f698d7c908b
yasiru2077 <96358931+yasiru2077@users.noreply.github.com>
2026-01-14 13:00:40 +0530

    hackerrank practice

--- hackerrankPractice/Main.java
+++ hackerrankPractice/Main.java
@@ -1,4 +1,29 @@
 package hackerrankPractice;
 
+import java.util.Scanner;
+
 public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int q = in.nextInt();
+
+        for (int i = 1; i <= q; i++) {
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+
+            int totalSum = a;
+            for (int j = 0; j < n; j++) {
+                totalSum = (int) (totalSum + Math.pow(2,j)*b);
+                System.out.print(totalSum+ " ");
+            }
+
+            System.out.println();
+
+        }
+
+    }
+
 }

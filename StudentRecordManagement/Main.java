dfc413d113844e561096f1fdef67f605659a0f26
yasiru2077 <96358931+yasiru2077@users.noreply.github.com>
2026-01-16 20:04:23 +0530

    refactor:student record managemet

--- StudentRecordManagement/Main.java
+++ StudentRecordManagement/Main.java
@@ -3,19 +3,76 @@ package StudentRecordManagement;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         StudentRecordManagement hr = new StudentRecordManagement();
 
-        Record record = new Record("Ankit",7373,644);
+        Record record = new Record("Ankit", 7373, 644);
 
         hr.add(record);
 
         Scanner input = new Scanner(System.in);
-
-        int option = 0;
-
-
+        int option;
+        do {
+            menu();
+            option = input.nextInt();
+            switch (option) {
+                case 1:
+                    System.out.println("What is the Student id Number ? ");
+                    int idNumber = input.nextInt();
+                    System.out.println("What is the Student contact Number ? ");
+                    int contactNumber = input.nextInt();
+                    input.nextLine();
+                    System.out.println("What is the Student Name ? ");
+                    String name = input.nextLine();
+                    record = new Record(name,idNumber,contactNumber);
+                    hr.add(record);
+                    System.out.println(record);
+                    break;
+                case 2:
+                    System.out.println("What is the Student id number ? ");
+                    int rId = input.nextInt();
+                    hr.delete(rId);
+                    break;
+                case 3:
+                    System.out.println("What is the Student id number ? ");
+                    int rIdNo = input.nextInt();
+                    hr.update(rIdNo,input);
+                    break;
+                case 4:
+                    System.out.println("What is the Student id ? ");
+                    int bookId = input.nextInt();
+                    if(!hr.find(bookId)){
+                        System.out.println("Student id does not exit\n");
+                    }
+                    break;
+                case 5:
+                    hr.display();
+                    break;
+                case 9:
+                    System.out.println(
+                            "\nThank you for using the program. Goodbye!\n");
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("\nThank you using the program. Goodbye!\n");
+                    break;
+            }
+        } while (true);
 
 
     }
+
+    public static void menu() {
+        System.out.println("MENU");
+        System.out.println("1: Add Student");
+        System.out.println("2: Delete Student");
+        System.out.println("3: Update Student");
+        System.out.println("4: Search Student");
+        System.out.println("5: Display Students");
+        System.out.println("9: Exit program");
+        System.out.print("Enter your selection : ");
+    }
+
+
 }
+

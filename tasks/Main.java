05e62400f45f11f7f95cdda093e574248dcccc87
yasiru2077 <96358931+yasiru2077@users.noreply.github.com>
2026-01-19 22:31:10 +0530

    refactor

--- tasks/Main.java
+++ tasks/Main.java
@@ -10,16 +10,15 @@ public class Main {
 
         while(true){
             System.out.println("\n--- TASK MANAGER ---");
-            System.out.println("1. Add Task | 2. View Tasks | 3. Complete Task | 4. Delete Task | 5. Exit");
+            System.out.println("1. Add Task | 2. View Tasks | 3. Complete Task | 4. Delete Task | 5.Update | 6. Exit");
             int choice = scanner.nextInt();
             scanner.nextLine();
 
             switch (choice) {
                 case 1:
-
                     System.out.println("Type (1: Work, 2: Personal): ");
                     int type = scanner.nextInt();
-                    System.out.println("is Completed (1: done, 2: pending): ");
+                    System.out.println("is Completed (1: true, 2: false): ");
                     boolean completed = scanner.nextBoolean();
                     scanner.nextLine();
                     System.out.print("Enter task name: ");
@@ -28,7 +27,6 @@ public class Main {
                     break;
 
                 case 2:
-
                     manager.listTasks();
                     break;
                 case 3:
@@ -38,7 +36,12 @@ public class Main {
                 case 4:
                     System.out.println("Enter ID to delete:");
                     manager.deleteTask(scanner.nextInt());
+                    break;
                 case 5:
+                    System.out.println("Enter ID to Update");
+                    manager.update(scanner.nextInt());
+                    break;
+                case 6:
                     System.out.println("Goodbye!");
                     return;
             }
--- tasks/TaskManager.java
+++ tasks/TaskManager.java
@@ -1,32 +1,34 @@
 package tasks;
 
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class TaskManager {
 
     private List<Task> tasks = new ArrayList<>();
-    private int nextId =1;
+    private int nextId = 1;
 
-    public void addTask(String title,int type, boolean isCompleted){
-        Task newTask = (type == 1) ? new WorkTask(nextId++,title,isCompleted) : new PersonalTask(nextId++,title,isCompleted);
+    public void addTask(String title, int type, boolean isCompleted) {
+        Task newTask = (type == 1) ? new WorkTask(nextId++, title, isCompleted) : new PersonalTask(nextId++, title, isCompleted);
         tasks.add(newTask);
         System.out.println("Task added successfully.");
     }
 
-    public void listTasks(){
-        if (tasks.isEmpty()){
+    public void listTasks() {
+        if (tasks.isEmpty()) {
             System.out.println("No tasks found.");
             return;
         }
-        for (Task t:tasks){
+        for (Task t : tasks) {
             System.out.println(t);
         }
     }
 
-    public void completeTask(int id){
-        for (Task t:tasks){
-            if (t.getId() ==id){
+    public void completeTask(int id) {
+        for (Task t : tasks) {
+            if (t.getId() == id) {
                 t.setCompleted(true);
                 System.out.println("Task marked as complete");
                 return;
@@ -35,9 +37,31 @@ public class TaskManager {
         System.out.println("Task ID not found.");
     }
 
-    public void deleteTask(int id){
-        tasks.removeIf(t->t.getId()==id);
+    public void deleteTask(int id) {
+        tasks.removeIf(t -> t.getId() == id);
         System.out.println("Task deleted.");
+    }
+
+    public void update(int id){
+        Scanner scanner = new Scanner(System.in);
+
+        for (Task t:tasks){
+            if (t.getId() == id){
+                System.out.println(t.getId()+" "+t.getTaskType()+" "+t.getTitle());
+                System.out.println("What is the typ of this typ ? ");
+                int type = scanner.nextInt();
+                System.out.println("is it completed");
+                boolean isCompeleted = scanner.nextBoolean();
+                scanner.nextLine();
+                System.out.println("What is the title");
+                String title = scanner.nextLine();
+                Task updatedTask = (type == 1) ? new WorkTask(t.getId(),title,isCompeleted) : new PersonalTask(t.getId(),title,isCompeleted);
+                tasks.add(updatedTask);
+                System.out.println(updatedTask);
+                System.out.println("Task updated.");
+
+            }
+        }
     }
 
 }

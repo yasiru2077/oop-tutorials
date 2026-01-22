4b3ce36ad94fb7cecaa9ea0adee0e71e21782bf6
yasiru2077 <96358931+yasiru2077@users.noreply.github.com>
2026-01-22 22:30:19 +0530

    refactor exception

--- taskNew/Main.java
+++ taskNew/Main.java
@@ -6,28 +6,32 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         TaskManagerNew Newmanager = new TaskManagerNew();
         Scanner scanner = new Scanner(System.in);
 
-        while (true){
+        while (true) {
             System.out.println("\n--- Task Manager ---");
             System.out.println("1. Add task | 2. view tasks | 3. Complete task | 4. Delete tasks | 5. Update task | 6. Exits");
             int choice = scanner.nextInt();
             scanner.nextLine();
 
-            switch (choice){
+            switch (choice) {
 
                 case 1:
-                    System.out.println("Type (1: Work, 2: Personal): ");
-                    int type = scanner.nextInt();
-                    System.out.println("is Completed (1: true, 2: false): ");
-                    boolean completed = scanner.nextBoolean();
-                    scanner.nextLine();
-                    System.out.print("Enter task name: ");
-                    String name = scanner.nextLine();
-                    Newmanager.addTask(name, type, completed);
+                    try {
+                        System.out.print("Type (1: Work, 2: Personal): ");
+                        int type = Integer.parseInt(scanner.nextLine());
+                        System.out.print("is Completed (1: true, 2: false): ");
+                        boolean completed = Boolean.parseBoolean(scanner.nextLine());
+                        System.out.print("Enter task name: ");
+                        String name = scanner.nextLine();
+                        Newmanager.addTask(name, type, completed);
+                    } catch (Exception e) {
+                        System.out.println("Something went wrong!");
+                    }
+
                     break;
                 case 2:
                     Newmanager.listTasks();
